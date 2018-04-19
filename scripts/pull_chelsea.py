562a359503520ebba1d42f7646bea308b230fbcd
Chelsea <chelsea.dillon@jibo.com>
2018-04-19 11:40:08 -0400

    wohoo

--- scripts/pull_chelsea.py
+++ scripts/pull_chelsea.py
@@ -4,44 +4,6 @@ import paramiko
 import time
 from prettytable import PrettyTable
 
-
-# """
-# 	Grabs default robot name from jibo-cli
-# """
-# def get_robot_name():
-# 	robot_name = os.popen("jibo robot-list | grep '*'").read().rstrip()
-# 	robot_name = robot_name.split(' ')[-1]
-# 	return robot_name
-
-
-# """
-# 	Uses ssh connection to run wifi tool on robot.
-# 	Copies output.json from robot to local machine.
-# """
-# def run_tool(path, robot_name):
-# 	username = 'root'
-# 	password = 'jibo'
-# 	src = '/opt/.spooky_spy_stuff/robot_wifi_info.json'
-
-# 	ssh_client = paramiko.SSHClient()
-# 	ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-# 	ssh_client.connect(hostname=robot_name, username=username, password=password)
-
-# 	scan_wifi = 'python /opt/jibo/wifi-util/robot_wifi_info.py'
-# 	stdin, stdout, stderr = ssh_client.exec_command(scan_wifi)
-
-# 	exit_status = stdout.channel.recv_exit_status()
-# 	if exit_status == 0:
-# 		print("Scanning complete.")
-# 		ftp_client = ssh_client.open_sftp()
-# 		ftp_client.get(src, path)
-# 		ftp_client.close()
-# 	else:
-# 		print("Error", exit_status)
-
-# 	ssh_client.close()
-
-
 """
 	Grabs bssid's and signals from json file.
 	Creates a new json file with those key-values.
@@ -127,23 +89,19 @@ def print_signals(path, target_ssid, new_path):
 
 
 def main():
-	
-	path_json = sys.argv[1]
-	path_dir = sys.argv[2]
-	ssid = sys.argv[3]	
-	
-	path = os.path.expanduser('~/jibo/wifi_testing/{}'.format(path_json))
-	path_save = os.path.expanduser('~/jibo/wifi_testing/{}'.format(path_dir))
-	#if not os.path.exists(path):
-	#	os.makedirs(path)
 
-	files = os.listdir(path)
-	file_count = len(files)
+	path_dir = sys.argv[1]
+	ssid = sys.argv[2]	
+	
+	path_b = os.path.expanduser('~/jibo/wifi_testing/{}/before.json'.format(path_dir))
+	path_save_b = os.path.expanduser('~/jibo/wifi_testing/{}/b_report'.format(path_dir))
 
-	filename = 'wifi-scan-{}.json'.format(file_count)
-	#path_json = '{}/{}'.format(path, filename)
-	#run_tool(path_json, robot_name)
-	print_signals(path, ssid, path_save)
+	path_a = os.path.expanduser('~/jibo/wifi_testing/{}/after.json'.format(path_dir))
+	path_save_a = os.path.expanduser('~/jibo/wifi_testing/{}/a_report'.format(path_dir))
+
+	print_signals(path_b, ssid, path_save_b)
+	time.sleep(3)
+	print_signals(path_a, ssid, path_save_a)
 
 
 if __name__ == "__main__":
